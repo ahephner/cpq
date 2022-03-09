012b1db3853a3c09fa950ed6902b7e4a7af80d97
ahephner <ajhephner178@gmail.com>
2022-03-09 10:37:34 -0500

    submit for desktop sent for review

--- force-app/main/default/lwc/closeWinDesktop/closeWinDesktop.js
+++ force-app/main/default/lwc/closeWinDesktop/closeWinDesktop.js
@@ -1,6 +1,9 @@
+//NEED A SPINNER 
+
 import { LightningElement, api, wire } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getRecord, getFieldValue, updateRecord } from 'lightning/uiRecordApi';
 import NAME from '@salesforce/schema/Opportunity.Name';
 import QUOTENUM from '@salesforce/schema/Opportunity.Quote_Number_Test__c';
 import CLOSEDATE from '@salesforce/schema/Opportunity.CloseDate';
@@ -10,25 +13,28 @@ import DELIVERYDATE from '@salesforce/schema/Opportunity.Delivery_Date_s_Request
 import DELIVERDATE2 from '@salesforce/schema/Opportunity.To__c';
 import SHIPTO from '@salesforce/schema/Opportunity.Shipping_Address__c'
 import ACCID from '@salesforce/schema/Opportunity.AccountId';
+import ID_Field from '@salesforce/schema/Opportunity.Id';
+import REQPO from '@salesforce/schema/Opportunity.Requires_PO_Number__c';
 import getAddress from '@salesforce/apex/cpqApex.getAddress'
-const FIELDS = [NAME, QUOTENUM, CLOSEDATE, STAGE, PO,DELIVERYDATE, DELIVERDATE2, SHIPTO, ACCID]
+const FIELDS = [NAME, QUOTENUM, CLOSEDATE, STAGE, PO,DELIVERYDATE, DELIVERDATE2, SHIPTO, ACCID, REQPO]
 export default class CloseWinDesktop extends LightningElement {
     @api recordId;
     @api objectApiName; 
     msg = 'Adding an address can slow quote acceptance by up to 10 mins in the system'
     info=true; 
-    name 
+    name;
+    loaded=false
     quoteNumb; 
     closeDate;
     stage = 'Closed Won';
     po;
+    reqPO 
     deliveryDate;
     deliverDate2;
     accountId;
-    shipTo; 
-    options; 
-    localError;
-    errorMsg;
+    shipTo;
+    options;
+    errorMsg = {};
 
     @wire(getRecord,{recordId: '$recordId', fields:FIELDS})
         loadFields({data,error}){
@@ -42,13 +48,24 @@ export default class CloseWinDesktop extends LightningElement {
                 this.deliverDate2 = getFieldValue(data, DELIVERDATE2);
                 this.accountId = getFieldValue(data, ACCID);
                 this.shipTo = getFieldValue(data, SHIPTO); 
+                this.reqPO = getFieldValue(data, REQPO);
                 this.findAddress(this.accountId); 
+                this.loaded = true; 
             }else if(error){
                 let err = JSON.stringify(error);
                 alert(err)
             }
         }
-
+//Stage Options
+get stageOptions() {
+    return [
+        { label: 'Pricing', value: 'Pricing' },
+        { label: 'Working', value: 'Working' },
+        { label: 'Quote', value: 'Quote' },
+        { label: 'Closed Won', value: 'Closed Won' },
+        { label: 'Closed Lost', value: 'Closed Lost' },
+    ];
+}
     //get address stuff
     //get the avaliable ship to options
     findAddress(rec){
@@ -77,7 +94,8 @@ export default class CloseWinDesktop extends LightningElement {
         
         return label;   
 }
-
+//Change Sections
+//new Ship Address
 selectChange(event){
     let newValue = this.template.querySelector('.slds-select').value;
     if(newValue === "new"){ 
@@ -88,28 +106,38 @@ selectChange(event){
         
     }
 }
-//Stage Options
-get stageOptions() {
-    return [
-        { label: 'Pricing', value: 'Pricing' },
-        { label: 'Working', value: 'Working' },
-        { label: 'Quote', value: 'Quote' },
-        { label: 'Closed Won', value: 'Closed Won' },
-        { label: 'Closed Lost', value: 'Closed Lost' },
-    ];
-}
 
+//Stage Change
 handleStageChange(event) {
     this.stage = event.detail.value;
 }
+//New Name
+newName(e){
+    this.name = e.detail.value; 
+}
 
+newCloseDate(e){
+    this.closeDate = e.detail.value; 
+}
 
+newPo(e){
+    this.po = e.detail.value; 
+}
+
+newDeliveryDate(e){
+    this.deliveryDate = e.detail.value; 
+}
+
+newDevDate2(e){
+    this.deliverDate2 = e.detail.value; 
+}
 //Save Submit section!
 
     handleSave(){
-        let ok = this.valid();
+        
+        let ok = this.isInputValid();
         if(ok){
-            console.log('good')
+            this.loaded = false; 
             const fields = {}
             fields[NAME.fieldApiName] = this.name;
             fields[QUOTENUM.fieldApiName] = this.quoteNumb;
@@ -119,29 +147,59 @@ handleStageChange(event) {
             fields[DELIVERYDATE.fieldApiName] = this.deliveryDate;
             fields[DELIVERDATE2.fieldApiName] = this.deliverDate2;
             fields[SHIPTO.fieldApiName] = this.shipTo;
+            fields[ID_Field.fieldApiName] = this.recordId; 
+            const opp = {fields}
+            console.log(JSON.stringify(opp))
+            updateRecord(opp)
+                .then(()=>{
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Submitted',
+                            message: 'Order Sent In!',
+                            variant: 'success'
+                        })
+                    )
+                })
+                .then(()=>{
+                    this.loaded = true; 
+                    this.dispatchEvent(new CloseActionScreenEvent());
+                })
+                .catch(error=>{ 
+                    console.log(JSON.stringify(error));
+                    
+                    
+                    this.dispatchEvent(
+                        new ShowToastEvent({ 
+                            title: 'Error Updating',
+                            message: error.body.output.errors[0].message,
+                            variant:'error'
+                        })
+                    )
+                    this.loaded = true; 
+                })
             
         }else{
-            this.localError = true;
-            this.errorMsg = this.valid(); 
+           console.log(this.errorMsg)
         }
     }
     handleCancel(){
         this.dispatchEvent(new CloseActionScreenEvent());
     }
-    valid(){
-        let mess;
-        console.log(1,this.shipTo, 2, this.deliveryDate);
-        
-        if(this.shipTo === null || !this.shipTo ){
-            mess = 'Add a ship to';
-            return mess; 
-        }else if(this.deliveryDate === null || !this.deliveryDate){
-            mess = 'Add a delivery date'
-            return mess; 
-        }else{
-            return; 
-        } 
-        
+    isInputValid() {
+        let isValid = true;
+        let inputFields = this.template.querySelectorAll('lightning-input');
+        const ship = this.template.querySelector('.valAdd');
+        inputFields.forEach(inputField => {
+            if(!inputField.checkValidity()) {
+                inputField.reportValidity();
+                isValid = false;
+            }else if(!ship.checkValidity()){
+                ship.reportValidity(); 
+                isValid = false; 
+            }
+            this.errorMsg[inputField.name] = inputField.value;
+        });
+        return isValid;
     }
 //New Address info
         cancelNewAddress(){

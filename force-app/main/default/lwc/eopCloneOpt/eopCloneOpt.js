1bd6e53ccaeb2e04cf25b07c8a332a577700dd3c
ahephner <ajhephner178@gmail.com>
2022-07-20 08:45:27 -0400

    added mobile aura to mobileEOPClone requires user to select if a cloned opportunity is EOP or not. Add Apex class that sets EOP status. Both mobile and desktop use same lwc component eopCloneOpt to do cloning logic and call apex class

--- force-app/main/default/lwc/eopCloneOpt/eopCloneOpt.js
+++ force-app/main/default/lwc/eopCloneOpt/eopCloneOpt.js
@@ -1,11 +1,21 @@
 import { LightningElement, api } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
 import { NavigationMixin } from 'lightning/navigation';
+import FORM_FACTOR from '@salesforce/client/formFactor';
 import eopClone from '@salesforce/apex/cpqOppClone.cloneOppEOP';
 export default class EopCloneOpt extends NavigationMixin(LightningElement) {
     @api recordId; 
     loaded = true;
     value;
+    formSize; 
+
+    connectedCallback() {
+      this.formSize = this.screenSize(FORM_FACTOR);  
+    }
+        //check screen size to show table on desktop and cards on mobile
+    screenSize = (screen) => {
+        return screen === 'Large'? true : false
+    }
     get options(){
         return [
             {label: 'Yes', value:'Yes'},
@@ -58,4 +68,12 @@ export default class EopCloneOpt extends NavigationMixin(LightningElement) {
         })
         return valid; 
     }
+
+    mCancel(){
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    // mSave(){
+    //     this.save(); 
+    // }
 }

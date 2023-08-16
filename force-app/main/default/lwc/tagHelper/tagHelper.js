919b3626de522200fba7ebe6b96964bd0b292c2b
ahephner <ajhephner178@gmail.com>
2023-08-16 15:08:37 -0400

    able to query mobile get back results and parse multiple tags to one. Need to maybe update the mobile query to allow for fields in the mobile app display that are differnt than desktop requirements

--- force-app/main/default/lwc/tagHelper/tagHelper.js
+++ force-app/main/default/lwc/tagHelper/tagHelper.js
@@ -16,15 +16,15 @@ const quickSearchString = (term, stock)=>{
     return searchString; 
   }
 
-const cpqSearchString = (term, stock) =>{
-  let searchString = 'FIND \''+term+'\' IN ALL FIELDS RETURNING Tag__c(id, Tag_Description__c, Search_Slug_2__c, '
-  +'Product__c, Product_Name__c, Product__r.Temp_Unavailable__c,Product__r.Temp_Mess__c, ATS_Score__c, Stock_Status__c, '
-  +'W_Focus_Product__c, W_Product_Profitability__c, W_Program_Score__c, W_Inventory_Score__c, '
-  +'Floor_Price__c, Product__r.Total_Product_Items__c,Product__r.Floor_Type__c, Product_Code__c where product__r.IsActive = true';
-
-stock != null ? searchString += ' and Stock_Status__c  = \''+stock+'\' order by ATS_Score__c desc nulls last)' :searchString += ' order by ATS_Score__c desc nulls last)'; 
-return searchString; 
-}  
+  const cpqSearchString = (term, stock) =>{
+    let searchString = 'FIND \''+term+'\' IN ALL FIELDS RETURNING Tag__c(id, Tag_Description__c, Search_Slug_2__c, '
+    +'Product__c, Product_Name__c, Product__r.Temp_Unavailable__c,Product__r.Temp_Mess__c, ATS_Score__c, Stock_Status__c, '
+    +'W_Focus_Product__c, W_Product_Profitability__c, W_Program_Score__c, W_Inventory_Score__c, '
+    +'Floor_Price__c, Product__r.Total_Product_Items__c,Product__r.Floor_Type__c, Product_Code__c where product__r.IsActive = true';
+  
+  stock != null ? searchString += ' and Stock_Status__c  = \''+stock+'\' order by ATS_Score__c desc nulls last)' :searchString += ' order by ATS_Score__c desc nulls last)'; 
+  return searchString; 
+  }
 
 const uniqVals = (arr, track = new Set())=>{
   
@@ -37,3 +37,12 @@ export{
        cpqSearchString,
        uniqVals
       }
+
+
+///SEARCH STRING FOR ADDING IN STATUS FOR SEARCH
+    //   let searchString = `FIND '${term}' IN ALL FIELDS RETURNING Tag__c(id, Tag_Description__c, Search_Slug_2__c,`
+    //   +`Product__c, Product_Name__c, Product__r.Temp_Unavailable__c,Product__r.Temp_Mess__c, ATS_Score__c, Stock_Status__c, `
+    //   +`W_Focus_Product__c, W_Product_Profitability__c, W_Program_Score__c, W_Inventory_Score__c, Floor_Price__c,`
+    //   +`Product__r.Total_Product_Items__c,Product__r.Floor_Type__c, Product_Code__c where product__r.IsActive = true and Tag_Status__c = 'Active'`;
+    //   stock != null ? searchString += `and Stock_Status__c  = '${stock}' order by ATS_Score__c desc nulls last)` :searchString +=` order by ATS_Score__c desc nulls last)`
+    // return searchString; 
